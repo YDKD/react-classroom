315d0c8313487e429f33f53ae21421de0b3cb40b
YDKD <1606354057@qq.com>
2022-11-09 09:04:06 +0800

    feat(home): add homm banner

---
+++ src/views/home/components/home-banner/styled.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+import coverBg from '@/assets/img/cover.jpg'
+
+const HomeBannerWrapper = styled.div`
+  .home-bg {
+    height: 529px;
+    background: url(${coverBg}) no-repeat center center;
+  }
+`
+
+export default HomeBannerWrapper
