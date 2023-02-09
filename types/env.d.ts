9f6e3592c562bb4c2751eb4242fa72572ce8139a
Coder YDKD <1606354057@qq.com>
2023-02-09 09:28:04 +0800

    docs(formatter): formatter docs

--- types/env.d.ts
+++ types/env.d.ts
@@ -2,6 +2,6 @@
  * vite meta env
  */
 interface ImportMetaEnv {
-  readonly VITE_ENV: "development" | "production"
+  readonly VITE_ENV: 'development' | 'production'
   readonly VITE_REQUEST_URL: string
 }
