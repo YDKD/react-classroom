ed46196f63352c56a10f393e47c39f5afd43c379
YDKD <1606354057@qq.com>
2023-03-23 00:07:11 +0800

    feat(video): complete detail video play

--- src/views/video-detail/index.tsx
+++ src/views/video-detail/index.tsx
@@ -4,6 +4,8 @@ import { memo, useEffect } from 'react'
 import { shallowEqual, useDispatch, useSelector } from 'react-redux'
 import { useLocation } from 'react-router-dom'
 import { IVideoListItem } from '../home/types'
+import Player from 'xgplayer'
+import config from '@/config'
 
 const videoDetail = memo(() => {
   const dispatch = useDispatch<AppDispatch>()
@@ -27,7 +29,15 @@ const videoDetail = memo(() => {
     dispatch(getVideoAction(videoId))
   }, [])
 
-  return <div>{videoData?.title}</div>
+  useEffect(() => {
+    console.log('videoData', videoData?.url || '')
+    const player = new Player({
+      id: 'mse',
+      url: config.fileUploadUrl + videoData?.url
+    })
+  }, [videoData])
+
+  return <div id="mse"></div>
 })
 
 export default videoDetail
